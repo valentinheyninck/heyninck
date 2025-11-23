d0463ff2eb343c3af93c77e60acf9ff84dac9852
valentinheyninck <valentinheyninck@icloud.com>
2025-11-24 00:28:58 +0100

    Fix install path for macOS ARM

--- install.sh
+++ install.sh
@@ -8,8 +8,14 @@ curl -L https://github.com/valentinheyninck/heyninck/releases/latest/download/he
 # Donner les droits
 chmod +x /tmp/heyninck
 
-# Déplacer dans un dossier du PATH
-sudo mv /tmp/heyninck /usr/local/bin/heyninck
+# Détecter le bon dossier du PATH
+if [ -d "/opt/homebrew/bin" ]; then
+    TARGET="/opt/homebrew/bin/heyninck"
+else
+    TARGET="/usr/local/bin/heyninck"
+fi
+
+sudo mv /tmp/heyninck "$TARGET"
 
 echo "Installation terminée !"
 echo "Tu peux maintenant lancer : heyninck"
